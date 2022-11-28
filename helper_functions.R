daa7418ed44974f5216332d322a98678c310128e
jlgrossmann <118946066+jlgrossmann@users.noreply.github.com>
2022-11-28 16:32:31 +0100

    initial commit

---
+++ helper_functions.R
@@ -0,0 +1,231 @@
+# scaling correction methods based on assessor model and generalised prodcrustes analysis
+
+
+# generate data to try scaling adjustment methods
+# I=4, J=5, K=2
+test_data = list(readings=rep(unlist(sapply(1:10, function(x) 1:5*x, simplify=F)), each=2)+rnorm(100,1), 
+                 assessors=rep(1:10, each=10), 
+                 samples=rep(rep(1:5, 10), each=2), 
+                 replicates=rep(1:2, 50))
+# ten_berge(test_data$readings, test_data$assessors, test_data$samples, test_data$replicates)
+
+
+ten_berge = function(readings, assessors, samples, replicates, na.remove=T, na.compensate=F, prnt=F){
+    #' calculate ten Berge scaling factors
+    #' input: 4 vectors of equal length
+    #' intended for balanced datasets
+    #' readings = attribute values
+    #' alculations are done based on averaged replicates
+    
+    df_ = data.frame(readings=readings, assessors=assessors, samples=samples, replicates=replicates)
+    df_ = df_[order(assessors, samples, replicates),]
+    
+    n_assessors = length(unique(assessors))
+    n_samples = length(unique(samples))
+    n_replicates = length(unique(replicates))
+    
+    if(nrow(df_) < n_assessors*n_samples*n_replicates) warning('ten Berge: dataset is not balanced')
+
+    # average replicates
+    df_$readings_mean = ave(df_$readings, paste(df_$assessors, df_$samples), FUN=mean)
+    df_ = df_[!duplicated(paste(df_$assessors, df_$samples)),]
+    
+    if(nrow(df_) < n_assessors*n_samples) stop('ten Berge: dataset misses sample-assessor combinations')
+    
+    # rows = assessors, cols = samples
+    Y = reshape2::acast(df_, assessors~samples, value.var='readings_mean')
+    # subtract assessor means
+    Y = sweep(Y, 1, rowMeans(Y), "-")
+    rown = rownames(Y)
+    
+    
+    RR = sum(Y^2, na.rm=T)  # sum of squares
+    
+    Y = sqrt(n_assessors/RR)*Y  # scale Y so that total sqrt(SSQ) = sqrt(n_assessors)
+    
+    # pairwise inner products between assessors
+    YY = matrix(nrow=n_assessors, ncol=n_assessors)  
+    
+    for(ass in seq_along(rown))
+        for(ass_ in seq_along(rown))
+            if(ass<=ass_)
+                YY[ass==unique(assessors), ass_==unique(assessors)] = 
+                    YY[ass_==unique(assessors), ass==unique(assessors)] = 
+                        sum(diag(t(Y[ass==rown,]) %*% Y[ass_==rown,]), na.rm=T)
+            
+    
+    # for scaling by assessor
+    Yd = diag(diag(YY), n_assessors, n_assessors)
+    
+    # scaling of rows and columns
+    Q = expm::sqrtm(solve(Yd)) %*% YY %*% expm::sqrtm(solve(Yd))
+    
+    EQ = eigen(Q)
+    PQ = EQ$vectors
+    
+    # factor G for each assessor
+    # for correction, the readings of each assessor must be multiplied with the respective G
+    G = sapply(1:n_assessors, function(x) (n_assessors/Yd[x,x])**(1/2)*PQ[x,1])
+    names(G) = unique(assessors)
+    if(mean(G)<0) G=-G
+    
+    return(G)
+}
+
+
+assessor_mod = function(readings, assessors, samples, replicates){
+    #' estimate the assessor model according to Brockhoff&Skovgaard (1994) 
+    #'     doi: 10.1016/0950-3293(94)90037-X
+    #' and adjust readings according to Romano et al (2008)
+    #' data is assumed to be ordered by (assessor, sample, replicate)      
+    
+    n_samples = length(unique(samples))
+    n_assessors = length(unique(assessors))
+    n_replication = length(unique(replicates))
+    readings_mult = readings
+    readings_add = readings
+    
+    # initialise product effects nu_p 
+    # calculate product means
+    Y.p. = aggregate(readings, by=list(samples), FUN=mean)[2]
+    
+    # mean intensity of assessors (alpha)
+    alpha = aggregate(readings, by=list(assessors), FUN=mean)[2]
+    
+ 
+    ####### Step 1:Initialize Va (called nu_p in paper) ########
+    
+    nu_p = as.matrix((Y.p.-mean(readings)) / sqrt(sum((Y.p.-mean(Y.p.[,1]))^2)) )
+    nu_p_est = matrix(nrow=n_samples, ncol=1)
+    
+    #############Step 2: Paramater estimations##################
+    D_V = 2
+    while(D_V>1e-7){
+        
+        ## estimate beta_a
+        Beta = matrix(nrow=n_assessors, ncol =1)
+        Yap = matrix(nrow=n_assessors, ncol=n_samples)
+        
+        for(a in 1:n_assessors){ 
+            Sum_Beta = 0
+            for(p in 1:n_samples){
+                Yap[a,p]= mean(readings[assessors==a & samples==p])
+                Sum_Beta = Sum_Beta + Yap[a,p]*nu_p[p,]
+            }
+            Beta[a,] = Sum_Beta
+        }
+        
+        ## estimate sigma2_a
+        sigma2_a = matrix(nrow=n_assessors, ncol =1)
+        for(a in 1:n_assessors){
+            sum_PR=0
+            for(p in 1:n_samples){
+                sum_PR = sum_PR + 
+                    sum((readings[assessors==a & samples==p]-alpha[a,]-Beta[a,]*nu_p[p,])**2) 
+            }
+            sigma2_a[a,] = sum_PR/(n_samples*n_replication)
+        }
+        
+        ## eq 11 (step 3)
+        for(p in 1:n_samples){
+            Sum_Beta_Sigma_Y = 0
+            Sum_Beta_Sigma = 0
+            
+            for(a in 1:n_assessors){
+                Sum_Beta_Sigma  = Sum_Beta_Sigma + Beta[a,]**2/sigma2_a[a,]
+                Sum_Beta_Sigma_Y = Sum_Beta_Sigma_Y + (Beta[a,]**2/sigma2_a[a,])*((Yap[a,p] - alpha[a,])/(Beta[a,]))
+            }
+            nu_p_est[p,] = Sum_Beta_Sigma_Y/Sum_Beta_Sigma
+        }
+        
+        ## step 4: new estimate for nu_p; calculate change
+        SV = (nu_p_est - mean(nu_p_est))/sqrt(sum( (nu_p_est - mean(nu_p_est))^2 ))
+        D_V = (t(SV-nu_p)%*%(SV-nu_p))
+        nu_p = SV
+        if(!is.numeric(D_V)) return(list(status=F))
+        # catch error
+    }
+    
+    # multiplicative scaling (Romano et al., 2008, Eq. 10)
+    for(i in 1:n_assessors)
+        readings_mult[assessors==i] = (readings[assessors==i] - alpha[i,])/(Beta[i,])
+    
+    # additive scaling (remove only scaling part of interaction) (Romano et al., 2008, Eq. 11)
+    for(i in 1:n_assessors){
+        for(j in 1:n_samples){
+            readings_add[assessors==i & samples==j] = 
+                readings[assessors==i & samples==j] - (Beta[i,]-mean(Beta))*nu_p_est[j]
+        }
+    }
+    return(list(mult=readings_mult, add=readings_add, Beta=Beta, status=T))
+}        
+
+
+
+fit_MAM = function(sim_dat){
+    ################# MAM ###################
+    
+    # options for MAM: 
+    #  1)  adjustedMAM or normal MAM: how to deal with negative scaling?
+    #  2)  conditionalMAM: use MAM or AOV, depending on p(scaling)
+    # resulting options: 
+    # - MAM.AOV_default: adjusted MAM/AOV with p(scaling) = 0.2 [this is the default case] - called MAM_CA in paper
+    # - MAM_adj: adjusted MAM with p(scaling) = 1, thus always use MAM, and adjust SS(scaling) - called MAM_A following paper nomenclature
+    # - MAM.AOV_nonadj: non-adjusted MAM/AOV with p(scaling) = 0.2 - called MAM_C in paper
+    # - MAM_nonadj: non-adjusted MAM/AOV with p(scaling) = 1, thus always MAM - called MAM following paper nomenclature
+    #  alpha_conditionalMAM = 0 is identical to using conventional ANOVA aov()
+
+    fit_MAM.AOV_default = MAManalysis(sim_dat[,c(2:4,1)], adjustedMAM=T, alpha_conditionalMAM=0.2)  # required column order: assessor, product, replicate, intensity
+    fit_MAM_adj = MAManalysis(sim_dat[,c(2:4,1)], adjustedMAM=T, alpha_conditionalMAM=1)  # required column order: assessor, product, replicate, intensity
+    fit_MAM.AOV_nonadj = MAManalysis(sim_dat[,c(2:4,1)], adjustedMAM=F, alpha_conditionalMAM=0.2)  # required column order: assessor, product, replicate, intensity
+    fit_MAM_nonadj = MAManalysis(sim_dat[,c(2:4,1)], adjustedMAM=F, alpha_conditionalMAM=1)  # required column order: assessor, product, replicate, intensity
+
+    
+    mam_f = sapply(list(fit_MAM.AOV_default, fit_MAM_adj, fit_MAM.AOV_nonadj, fit_MAM_nonadj), function(x) x$aov[,,1][2,4])
+    
+    mam_p = sapply(list(fit_MAM.AOV_default, fit_MAM_adj, fit_MAM.AOV_nonadj, fit_MAM_nonadj), function(x) x$aov[,,1][2,5])
+    
+    # record details about the MAM: p(scaling) in MAM_CA, p(scaling) in MAM_C, p(disagreement) in MAM_CA, 
+    #   p(disagreement) in MAM_C, number of negative scaling factors in MAM_CA
+    mam_extra = c(fit_MAM.AOV_default$aov[,,1][3,5], fit_MAM.AOV_nonadj$aov[,,1][3,5], fit_MAM.AOV_default$aov[,,1][4,5], 
+                 fit_MAM.AOV_nonadj$aov[,,1][4,5], fit_MAM.AOV_default$adj.MAM.negatives)  
+    
+    return(list(mam_f=mam_f, mam_p=mam_p, mam_extra=mam_extra))
+}
+    
+
+scaling_fit_aov = function(sim_dat) {  
+    # raw
+    sim_dat$raw = sim_dat$intensity
+    
+    
+    # standardisation
+    sim_dat$std = (sim_dat$intensity - ave(sim_dat$intensity, sim_dat$assessor, FUN=function(x) mean(x))) / ave(sim_dat$intensity, sim_dat$assessor, FUN=function(x) sd(x))
+    
+    
+    # Ten Berge 
+    # calculate scaling factors for assessors
+    tenb_facs = ten_berge(sim_dat$intensity, sim_dat$assessor, sim_dat$sample, sim_dat$repl)
+    
+    # transform according to tenberge factors
+    sim_dat$tenb = sim_dat$intensity - ave(sim_dat$intensity, sim_dat$assessor, FUN=mean)  # subtract means
+    sim_dat$tenb = (tenb_facs[sim_dat$assessor]*sim_dat$tenb) + mean(sim_dat$intensity)  # scale and restore original assessor means
+    
+    
+    # Assessor model
+    asm = assessor_mod(sim_dat$intensity, sim_dat$assessor, sim_dat$sample, sim_dat$repl)
+    if(asm$status==F) {Test = "NA"; print('error in assessor model'); next}  # error in estimating assessor model: try new permutation
+    sim_dat$add = asm$add
+    sim_dat$mult = asm$mult
+
+    
+    method_names = c('raw', 'std', 'tenb', 'add', 'mult')
+    
+    # ANOVA 
+    aov_fits = sapply(method_names, function(x) summary(aov(sim_dat[,x] ~ sim_dat$sample + Error(sim_dat$assessor/sim_dat$sample))), simplify=F)
+    aov_f = sapply(aov_fits, function(x) x[[2]][[1]][1,'F value'])
+    aov_p = sapply(aov_fits, function(x) x[[2]][[1]][1,'Pr(>F)'])
+    return(list(aov_f=aov_f, aov_p=aov_p, status=asm$status))
+}
+
+
---
+++ permutation_simulation.R
@@ -0,0 +1,204 @@
+# supplementary R code to accompany
+# JL Großmann et al: "Critical evaluation of assessor difference correction approaches in sensory analysis"
+# the following code is intented to illustrate the scaing correction, model fitting, permutation and 
+# simulation strategies employed in the aforementioned paper 
+
+# helper_functions.R contains functions for scaling correction and model fitting
+source('helper_functions.R')
+# MAManalysis.R is derived from the SensMixed R package by A Kuznetsova, PB Brockhoff and RHB Christensen 
+source('MAManalysis.R')
+starttime = format(Sys.time(), "%Y-%m-%d_%H.%M.%S")
+library(getopt)  # argument parsing
+
+# this script can be used to replicate the results of the permutation and simulation
+# approaches laid out in JL Großmann et al.
+# working directory must contain this script, helper_functions.R, MAManalysis.R and cheese.csv
+# options can either be hard-coded below or set as arguments in command line:
+# Rscript permutation_simulation.R --mode=permutation --nsim=10000 --ratt 13 --ptyp=ass
+#   to perform restricted permutation on Fattiness atribute, 10000 iterations
+# Rscript permutation_simulation.R --mode=simulation --nsim=10000 --simm=random_equal
+#   to perform simulation with equal sigma between assessors, 10000 iterations
+
+
+
+######## PARAMETERS #########
+# strategy to employ to obtain null distributions
+mode. = 'permutation'  # options: 'permutation' - will permute selected attribute of cheese dataset
+                       #          'simulation' - will generate random data 
+n_sim = 1e2  # Number of permutation / simulation iterations
+
+# for permutation
+perm_type = 'ass'  # how to permute 
+                   # options: 'ass' - "restricted permutation"
+                   #          'assrepl'
+                   #          'all' - "unrestricted permutation"
+sens_attribute = 13  # column index of real dataset attribute to permute (1-13 for cheese dataset)
+
+
+# for simulations (random data generation)
+simulation_mode = 'random_equal'  # how to simulate 
+                                  # options: 'random_equal' - equal sigma_i between assessors
+                                  #          'random_unequal' - unequal sigma_i between assessors, sampled from uniform distribution
+n_ass = 12  # I
+n_samp = 14  # J
+n_rep = 2   # K
+
+#############################
+
+
+# read command line arguments. options specified in command line will overwrite options hard-coded above
+spec = matrix(c(
+  'nsim', 'n', 2, "double",    # number of iterations
+  'nass', 'i', 2, "double",    # number of assessors
+  'nsam', 'j', 2, "double",    # number of samples
+  'nrep', 'k', 2, "double",    # number of replicates
+  'mode', 'm', 2, "character", # for permutations: use real data?
+  'ratt', 'u', 2, "integer",   # column to use in real dataset 
+  'ptyp', 't', 2, "character", # permutation type  ('ass', 'repl', 'assrepl', 'all')
+  'simm', 's', 2, "character"   # simulation mode
+), byrow=TRUE, ncol=4)
+opt = getopt(spec)
+
+aliases = c("nsim"="n_sim", "mode"="mode.", "nass"="n_ass", "nsam"="n_samp", "nrep"="n_rep",
+            "ratt"='sens_attribute', "ptyp"="perm_type", "simm"="simulation_mode")
+
+# overwrite options if set
+for(arg_ in names(opt)[!names(opt) %in% c("ARGS", "taskid")])
+  assign(aliases[names(aliases)==arg_], opt[[arg_]])
+
+
+
+
+
+
+if(mode.=='permutation'){
+  cheese = read.csv('cheese.csv')
+  cheese_attributes = names(cheese)[-(1:3)]
+  if(sens_attribute < 1 | sens_attribute > 13) stop ('attibute index out of range')
+  
+  sens_attribute = cheese_attributes[sens_attribute]
+  real_dat = data.frame(intensity=cheese[,sens_attribute],
+                        assessor=as.factor(cheese$Assessor),
+                        sample=as.factor(cheese$Sample),
+                        repl=as.factor(cheese$Replicates))
+
+  n_ass = length(unique(real_dat$assessor))
+  n_samp = length(unique(real_dat$samp))
+  n_rep = length(unique(real_dat$repl))
+  perm = T 
+  simulation_mode = ''
+} else if (mode.=='simulation'){
+  perm = F
+  perm_type = ''
+} else {
+  stop('set mode to simulation or permutation')
+}
+
+cat('###   settings   ###\n')
+if(mode.=='permutation') {
+  cat(paste0('permuting cheese data, attribute: ', sens_attribute, 
+             '\npermutation mode: ', perm_type))
+  
+} else { 
+  cat(paste0('simulating data', 
+             '\nn_ass\t\t', n_ass, 
+             '\nmode\t\t', simulation_mode, 
+             '\nn_samp\t\t', n_samp, 
+             '\nn_rep\t\t', n_rep))
+}
+cat(paste0('\nnumber of iterations: ', n_sim))  
+print(  perm)
+print(mode.)
+
+outfile = paste0('out_', mode., '_', 
+                 ifelse(mode.=='permutation', paste0(perm_type, '_cheese-', sens_attribute), 
+                        paste0(simulation_mode, '_I', n_ass, '_J', n_samp, '_K', n_rep)),
+                 '_', starttime, '.Rds')
+cat(paste0('\nwriting to: ', paste0(getwd(), '/', outfile), '\n'))
+
+
+# tables holding permutation / simulation results
+f_table = p_table = matrix(nrow=n_sim, ncol=9, dimnames=list(1:n_sim, 
+      c('raw', 'std', 'tenb', 'add', 'mult', 'MAM.AOV_default', 'MAM_adj', 'MAM.AOV_nonadj', 'MAM_nonadj')))
+
+# additional outputs of the MAM
+mam_extra = matrix(nrow=n_sim, ncol=5, dimnames=list(NULL, c('p_scaling_default', 'p_scaling_nonadj', 'p_disagree_default', 'p_disagree_nonadj', 'n_negatives')))
+
+
+# permutation: use real (cheese) data
+if(perm)
+    sim_dat = real_dat  # inject real data
+
+
+for(ns in 1:n_sim){  # simulation / permutation iterations
+  if (ns %% 10 == 0) cat('iteration ', ns, '\n')
+  status = F
+  
+  while(!status){
+    
+    if(perm & ns > 1) {  # real data: perform permutation
+                         # 1st row of results table is UNpermuted data!
+      if (perm_type == 'ass'){  
+        # permute within assessors ("restricted permutation")
+        # underlying model: y_{ijk} = mu + a_i + epsilon_{ijk}; epsilon ~ N(0,sigma_i^2)
+        for (ass in unique(sim_dat$assessor)){
+          ass_idx = which(sim_dat$assessor == ass)
+          ass_idx_perm = sample(ass_idx, length(ass_idx), replace=F)
+          sim_dat[ass_idx, 'intensity'] = sim_dat[ass_idx_perm, 'intensity']
+        }
+      } else if (perm_type == 'assrepl'){  
+        # keep replicates and assessors together [not examined in the paper; not a feasible null hypothesis]
+        # $y_{ijk} = mu + a_i + g_{ij} + epsilon_{ijk}; epsilon_{ijk} ~ N(0,\sigma_{ij}^2
+        for (ass in unique(sim_dat$assessor)){
+          ass_idx = which(sim_dat$assessor == ass)
+          idx_ = sample(n_samp)
+          ass_idx_perm = c(rbind(2*idx_-1, 2*idx_))
+          sim_dat[ass_idx, 'intensity'] = sim_dat[ass_idx[ass_idx_perm], 'intensity']   # permute but keep replicates and assessors together
+        } 
+      } else if (perm_type == 'all'){  
+        # permutation across all values (completely random, "unrestricted permutation")
+        # underlying model: y_{ijk} = mu + epsilon_{ijk}; epsilon ~ N(0,sigma^2)
+        sim_dat$intensity = sample(sim_dat$intensity) 
+      } else stop('invalid permutation type')
+    }
+    
+    if(mode.=='simulation') {  
+      if(simulation_mode=='random_equal') {  # mu = 5, sigma=1.5
+        sim_dat = data.frame(intensity=rnorm(n_ass*n_samp*n_rep, mean=5, sd=1.5),
+                             assessor=as.factor(rep(1:n_ass, each=n_samp*n_rep)),
+                             sample=as.factor(rep(1:n_samp, each=n_rep)),
+                             repl=1:n_rep)
+      
+      } else if(simulation_mode=='random_unequal') {  # mu = 5, sigma=unif(0.1,1)
+        sim_dat = data.frame(intensity=unlist(sapply(runif(n_ass, 0.1, 1), function(x) rnorm(n_samp*n_rep, mean=5, sd=x), simplify=F)),
+                             assessor=as.factor(rep(1:n_ass, each=n_samp*n_rep)),
+                             sample=as.factor(rep(1:n_samp, each=n_rep)),
+                             repl=1:n_rep)
+      } else {stop('invalid simulation mode')}
+    }
+    
+    
+    
+    out_aov = scaling_fit_aov(sim_dat)
+    if(!out_aov$status) next
+    status = T
+    out_mam = fit_MAM(sim_dat)
+    f_table[ns,] = c(out_aov$aov_f, out_mam$mam_f)
+    p_table[ns,] = c(out_aov$aov_p, out_mam$mam_p)
+    mam_extra[ns,] = out_mam$mam_extra
+    
+  } # end while
+  
+
+  if ((10*ns / n_sim) %% 1 == 0 | (10*ns / (n_sim-1)) %% 1 == 0 | ns == n_sim){  # save at every 10% progress
+    cat(paste('checkpoint at', ns, 'simulations. writing to disk:', outfile), '\n')
+    sim_results = list(f_table=f_table, p_table=p_table, mam_extra=mam_extra,
+                       parameters=list(mode.=mode., perm=perm, perm_type=perm_type, 
+                                       sens_attribute=sens_attribute,
+                                       simulation_mode=simulation_mode, n_ass=n_ass, n_samp=n_samp, n_rep=n_rep, n_sim=n_sim))
+    saveRDS(sim_results, paste0(outfile)) 
+  }
+}  
+
+
+print(paste('endtime:', format(Sys.time(), "%Y-%m-%d_%H.%M.%S")))
